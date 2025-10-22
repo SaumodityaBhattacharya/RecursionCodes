487bd1f01c0f719b007f17d2c6072ec29ca8def3
Saumoditya Bhattacharya <116937953+SaumodityaBhattacharya@users.noreply.github.com>
2025-10-22 20:57:43 +0530

    Dutch National Flag algorithm

---
+++ DNF.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DNF {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println();
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+                // mid++;
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
+}
